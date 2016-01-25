6ad8fa88d0e30e433446f14edb6d6ac75a3aeaa1
Binh <technobia@gmail.com>
2016-01-25 11:54:50 +0700

    rung npm global without sudo

---
+++ run-npm-global-without-sudo.sh
@@ -0,0 +1,9 @@
+sudo apt-get curl -y
+mkdir "${HOME}/.npm-packages"
+echo NPM_PACKAGES="${HOME}/.npm-packages" >> ${HOME}/.bashrc
+echo prefix=${HOME}/.npm-packages >> ${HOME}/.npmrc
+curl -L https://www.npmjs.org/install.sh | sh
+echo NODE_PATH=\"\$NPM_PACKAGES/lib/node_modules:\$NODE_PATH\" >> ${HOME}/.bashrc
+echo PATH=\"\$NPM_PACKAGES/bin:\$PATH\" >> ${HOME}/.bashrc
+echo source "~/.bashrc" >> ${HOME}/.bash_profile
+source ~/.bashrc
